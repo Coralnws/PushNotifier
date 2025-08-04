420149d3a8ff5c257780a030adc42771dc10f204
Coral <101916048+Coralnws@users.noreply.github.com>
2025-08-04 20:18:30 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/pushnotifier/main.go
@@ -0,0 +1,23 @@
+// cmd/pushnotifier/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"pushnotifier/internal/pushnotifier"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := pushnotifier.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
